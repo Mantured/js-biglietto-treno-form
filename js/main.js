1a076392209a54fcb2045fe8b3a6d94cb2f4a3f1
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-02-23 15:49:53 +0100

    new repo - mod style html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Il programma dovrà mostrare una form (a piacere) da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.27 € al km)
+va applicato uno sconto del 17% per i minorenni
+va applicato uno sconto del 33% per gli over 65.
+L'output del prezzo finale va visualizzato con massimo due decimali, per indicare centesimi sul prezzo.
+In questo caso richiederà un minimo di ricerca l'uso della select.
+Vi allego un'immagine esplicativa, ma potete usare la creatività e implementare layout diversi e creativi!! */
